a5342cce4d6ea05009e7777b10df03c45f1fa234
easyly471 <easyly@protonmail.com>
2019-01-12 15:12:02 +0300

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Regex-Matcher
+Regex matcher written in Java
+
+![img1](/screenshots/matcher1.PNG)
+![img2](/screenshots/matcher2.PNG)
---
+++ src/regexmatcher/RegexMatcher.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regexmatcher;
+
+import javax.swing.UIManager;
+import regexmatcher.mainScreen;
+/**
+ *
+ * @author simplex
+ */
+public class RegexMatcher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainScreen screen = new mainScreen();
+        screen.run(args);
+    }
+    
+}
